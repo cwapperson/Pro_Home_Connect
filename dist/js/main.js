454057dc6db0982cdc0aa5d1bb89ce9619b49673
Chris Apperson <cwapperson@gmail.com>
2020-01-07 16:51:53 -0600

    working on about me

--- dist/js/main.js
+++ dist/js/main.js
@@ -19,6 +19,8 @@ function toggleMenu() {
   }
 }
 
+// Function will auto close the navigation menu if it is
+// open and the size of the resolution changes
 function autoCloseMenu(x) {
   if (x.matches) {
     // If media query matches
@@ -30,3 +32,23 @@ function autoCloseMenu(x) {
 var x = window.matchMedia("(min-width: 500px)");
 autoCloseMenu(x); // Call listener function at run time
 x.addListener(autoCloseMenu); // Attach listener function on state changes
+
+// When the user scrolls the page, execute myFunction
+window.onscroll = function() {
+  myFunction();
+};
+
+// Get the header
+var header = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = header.offsetTop;
+
+// Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
